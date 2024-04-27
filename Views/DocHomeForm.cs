baee6f965f23070aa509a4cca33c48078e3261b4
marXus-3D <98558807+marXus-3D@users.noreply.github.com>
2024-04-27 16:18:26 +0300

    Sorting Doctors

--- Views/DocHomeForm.cs
+++ Views/DocHomeForm.cs
@@ -1,6 +1,7 @@
 ﻿using FontAwesome.Sharp;
 using Nova_Medical_Center.Data;
 using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace Nova_Medical_Center.Views
@@ -17,6 +18,7 @@ namespace Nova_Medical_Center.Views
             if (val == true)
             {
                 loadPanel.Visible = false;
+                InsertionSort(Data.Data.employees["Doctors"], emp => emp.First_Name);
                 doctorGridView.DataSource = Data.Data.employees["Doctors"];
             }
             else
@@ -30,6 +32,24 @@ namespace Nova_Medical_Center.Views
                 return;
             }
             DataLoader.LoadEmployees();
+        }
+        public static void InsertionSort<T>(List<T> list, Func<T, string> getProperty) where T : class
+        {
+            for (int i = 1; i < list.Count; i++)
+            {
+                T key = list[i];
+                int j = i - 1;
+
+                // Compare elements based on the property retrieved using the getProperty function
+                while (j >= 0 && getProperty(list[j]).CompareTo(getProperty(key)) > 0)
+                {
+                    list[j + 1] = list[j];
+                    j--;
+                }
+
+                // Insert the current element at its correct position
+                list[j + 1] = key;
+            }
         }
     }
 }
